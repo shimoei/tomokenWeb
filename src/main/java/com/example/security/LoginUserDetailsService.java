179e4b4846bd2dac34de96eaf2c9439956e724a3
shimoei <mshimoe1211@gmail.com>
2017-10-04 01:34:26 +0900

    デモ用にapiのurlを変更

--- src/main/java/com/example/security/LoginUserDetailsService.java
+++ src/main/java/com/example/security/LoginUserDetailsService.java
@@ -19,8 +19,9 @@ public class LoginUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
         //apiを叩いてユーザを検索する。
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8090/api/users/username?username=" + username;
-
+//        String url = "http://localhost:8090/api/users/username?username=" + username;
+        // デモ用に一時的にurlを変更させる。urlは将来的にはconfigファイルに書き出す
+        String url = "https://tomoken-api.herokuapp.com/api/users/username?username=" + username;
         UserForm userform = restTemplate.getForObject(url, UserForm.class);
         User user = new User(userform.getId(), userform.getUsername(), userform.getPassword(), userform.getMail());
 
